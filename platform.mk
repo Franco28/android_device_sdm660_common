c4abab7e936f0b5f52dde409078f03f8f7e45257
franco28 <franco.mato.contin@gmail.com>
2019-01-20 15:48:15 -0300

    DU-Wayne: updated keylayout

--- platform.mk
+++ platform.mk
@@ -220,11 +220,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(PLATFORM_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Keylayouts
-PRODUCT_COPY_FILES += \
-	$(PLATFORM_PATH)/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
-	$(PLATFORM_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
-
 # Lights
 PRODUCT_PACKAGES += \
 	android.hardware.light@2.0-service.xiaomi_sdm660
@@ -253,6 +248,13 @@ PRODUCT_PACKAGES += \
 	libOmxVdec \
 	libOmxVenc \
 	libstagefrighthw
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+	$(PLATFORM_PATH)/configs/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
+	$(PLATFORM_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+	$(PLATFORM_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
+	$(PLATFORM_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
 # Permissions
 PRODUCT_COPY_FILES += \
